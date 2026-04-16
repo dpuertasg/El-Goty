6840d4123205e0ecd0fced7cea352feeeb647c33
dpuertasg <dpuertasg@unicartagena.edu.co>
2026-04-16 14:22:18 -0500

    lo hice

---
+++ El-Goty/src/main/java/Juego/Ventana.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Juego;
+
+import java.awt.Canvas;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+/**
+ *
+ * @author USER
+ */
+public class Ventana extends Canvas{
+    private static final long serialVersionUID = 1L;
+    private static final int ANCHO = 800;
+    private static final int ALTO = 600;
+    private static final String NOMBRE = "theStore";
+    private static JFrame ventana;
+    
+    public Ventana(){
+        setPreferredSize(new Dimension(ANCHO,ALTO));
+        ventana = new JFrame(NOMBRE);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setResizable(false);
+        ventana.setLayout(new BorderLayout());
+        ventana.add(this,BorderLayout.CENTER);
+        ventana.pack();
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+    }
+}
---
+++ El-Goty/src/main/java/com/mycompany/el/goty/ElGoty.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.el.goty;
+import Juego.Ventana;
+/**
+ *
+ * @author USER
+ */
+public class ElGoty {
+
+    public static void main(String[] args) {
+      new Ventana();
+    }
+}
