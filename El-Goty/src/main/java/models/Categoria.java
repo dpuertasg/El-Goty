d4ea5f95fcae7a1c3fecb6c84fdf5c12fb0e5c49
JuanGirald <giraldojuangamarra@gmail.com>
2026-04-30 12:12:58 -0500

    Mas avanzes clases

--- El-Goty/src/main/java/models/Categoria.java
+++ El-Goty/src/main/java/models/Categoria.java
@@ -4,6 +4,7 @@
  */
 package models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -12,9 +13,59 @@ import java.util.List;
  * @author giral
  */
 public class Categoria {
-    private String nombreCat;
-    private int idCat;
+    private String nombre;
+    private int id;
     private List<Producto> productos;
+
+    public Categoria() {
+    }
     
     
+    public Categoria(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+        this.productos = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+    
+    public void crearCategoria(){
+        
+        
+    }
+    
+    public void agregarProductoCategoria (Producto p){
+        this.productos.add(p);
+    }
+    public List<Producto> obtenerProductosPorCategoria(){
+        return this.productos;
+    }
+   // regla de negocio "eliminarCategoria"
+    
+    public void eliminarCategoria(){
+    // falta un if y el codigo para eliminar la categoria
+    }
+   
 }
