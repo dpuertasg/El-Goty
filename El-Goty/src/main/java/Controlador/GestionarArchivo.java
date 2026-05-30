6f582082c38c1cd69fd8597192eb9bfd31430b44
dmaciasd1-ctrl <dmaciasd1@unicartagena.edu.co>
2026-05-29 21:58:16 -0500

    arregle el guardado

--- El-Goty/src/main/java/Controlador/GestionarArchivo.java
+++ El-Goty/src/main/java/Controlador/GestionarArchivo.java
@@ -13,36 +13,23 @@ import javax.swing.JOptionPane;
  * @author ESTUDIANTE
  */
 public class GestionarArchivo {
-   //escribir en el disco
-   public static void guardarJson(String rutaArchivo, String contenidoJson) {
+ 
+   // --- PASO 2: MÉTODO PARA ESCRIBIR EL TEXTO EN EL DISCO DURO ---
+    public static void guardarJson(String rutaArchivo, String contenidoJson) {
+        // Envolvemos en try-catch por seguridad de Entrada/Salida (I/O)
         try {
             File archivo = new File(rutaArchivo);
+            
             FileWriter escritor = new FileWriter(archivo);
-            escritor.write(contenidoJson);
+            escritor.write(contenidoJson);     
+            // Cerramos el archivo
             escritor.close(); 
             
-            System.out.println("¡Datos guardados con éxito en: " + archivo.getAbsolutePath() + "!");
-            
-            // --- 2. ¡AQUÍ AÑADIMOS LA VENTANA EMERGENTE! ---
-            // Parámetros: (Componente padre, Mensaje interno, Título de la ventana, Tipo de mensaje)
-            JOptionPane.showMessageDialog(
-                    null, 
-                    "¡Los datos del comprador se guardaron con éxito!", 
-                    "Guardado Exitoso", 
-                    JOptionPane.INFORMATION_MESSAGE
-            );
+            System.out.println("Datos guardados con exito en: " + archivo.getAbsolutePath() + "!");
             
         } catch (IOException e) {
-            System.out.println("Error crítico: No se pudo guardar el archivo JSON.");
+            System.out.println("Error critico: No se pudo guardar el archivo JSON.");
             e.printStackTrace();
-            
-            // --- Opcional: También podemos avisar si algo falló ---
-            JOptionPane.showMessageDialog(
-                    null, 
-                    "No se pudo guardar el archivo JSON.", 
-                    "Error de Guardado", 
-                    JOptionPane.ERROR_MESSAGE
-            );
         }
     }
 }
